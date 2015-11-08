cd062d37ca362cfedf6e256b5a9adf4b51ef4749
MahmoudHboubati <mhmoud.21830@gmail.com>
2015-11-08 20:10:51 +0400

    Some sort of testing units added
    in the current stage, very starter phase

--- js/tester.js
+++ js/tester.js
@@ -1,1 +1,20 @@
 //this is unit test file
+
+describe('gridController', function() {
+  beforeEach(module('app'));
+
+  var $controller;
+
+  beforeEach(inject(function(_$controller_){
+    // The injector unwraps the underscores (_) from around the parameter names when matching
+    $controller = _$controller_;
+  }));
+
+  describe('$scope.zero', function() {
+    it('returns zero value', function() {
+      var $scope = {};
+      var controller = $controller('gridController', { $scope: $scope });
+      expect($scope.zero();).toEqual(0);
+    });
+  });
+});
