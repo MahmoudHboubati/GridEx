cdf0af3458d513df6bb03e4664f4684f69732e59
MahmoudHboubati <mhmoud.21830@gmail.com>
2015-11-14 21:10:33 +0400

    format

--- js/GridEx.js
+++ js/GridEx.js
@@ -1,48 +1,61 @@
 var app = angular.module('gridApp', []);
 
-app.controller('gridController', function ($scope) {
-  
+app.controller('gridController', function($scope) {
+
   $scope.columnName = "col name";
   $scope.columns = [new column('Name'),
-                    new column('Age'),
-                    new column('BirthDate'),
-                    new column('Salary')];
-  $scope.getColumnName = function(colIndex){
+    new column('Age'),
+    new column('BirthDate'),
+    new column('Salary')
+  ];
+  $scope.getColumnName = function(colIndex) {
     var found = $scope.columns[colIndex]['name'];
     return found;
   }
 
-  $scope.addColumn = function(){
+  $scope.addColumn = function() {
     $scope.columns.push(new column(this.columnName));
   }
 
-  $scope.rows = [{Name:'Mahmoud', Age:'', BirthDate:'', Salary:1500},
-                 {Name:'Ammar', Age:'', BirthDate:'', Salary:1000},
-                 {Name:'Emran', Age:'', BirthDate:'', Salary:3000}]
+  $scope.rows = [{
+    Name: 'Mahmoud',
+    Age: '',
+    BirthDate: '',
+    Salary: 1500
+  }, {
+    Name: 'Ammar',
+    Age: '',
+    BirthDate: '',
+    Salary: 1000
+  }, {
+    Name: 'Emran',
+    Age: '',
+    BirthDate: '',
+    Salary: 3000
+  }]
 
   //get/set selected rows
   $scope.selected = [];
-  $scope.select = function(rowIndex){
+  $scope.select = function(rowIndex) {
     //Todo: check if already selected
     $scope.selected.push(rowIndex);
   }
-  $scope.unSelect = function(rowIndex){
+  $scope.unSelect = function(rowIndex) {
     if (rowIndex > -1) {
       $scope.selected.splice(rowIndex, 1);
     }
   }
 
   $scope.getOrderBy = function() {
-    if($scope.order[0])
-      {
-        alert($scope.getColumnName($scope.order[0]));
-        return $scope.getColumnName($scope.order[0]);
-      }
+    if ($scope.order[0]) {
+      alert($scope.getColumnName($scope.order[0]));
+      return $scope.getColumnName($scope.order[0]);
+    }
   };
 
   //order by columns indexes
   $scope.order = [];
-  $scope.orderBy = function(colIndex){
+  $scope.orderBy = function(colIndex) {
     $scope.order = [];
     alert(colIndex)
     $scope.order.push(colIndex);
